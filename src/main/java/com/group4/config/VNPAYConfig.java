72a6f3b58868b3d7f665a27de78be34c1aebed1b
DT256 <125669488+DT256@users.noreply.github.com>
2024-12-03 19:57:19 +0700

    Update VNPAYConfig.java

--- src/main/java/com/group4/config/VNPAYConfig.java
+++ src/main/java/com/group4/config/VNPAYConfig.java
@@ -32,22 +32,33 @@ public class VNPAYConfig {
 
     public Map<String, String> getVNPayConfig(String orderId) {
         Map<String, String> vnpParamsMap = new HashMap<>();
+
+        // Thông tin cấu hình VNPay
         vnpParamsMap.put("vnp_Version", this.vnp_Version);
         vnpParamsMap.put("vnp_Command", this.vnp_Command);
         vnpParamsMap.put("vnp_TmnCode", this.vnp_TmnCode);
         vnpParamsMap.put("vnp_CurrCode", "VND");
-        vnpParamsMap.put("vnp_TxnRef",  VNPayUtil.getRandomNumber(8));
-        vnpParamsMap.put("vnp_OrderInfo", "Thanh toan don hang:" +  VNPayUtil.getRandomNumber(8));
+        vnpParamsMap.put("vnp_TxnRef", VNPayUtil.getRandomNumber(8));
+        vnpParamsMap.put("vnp_OrderInfo", "Thanh toán đơn hàng:" + VNPayUtil.getRandomNumber(8));
         vnpParamsMap.put("vnp_OrderType", this.orderType);
         vnpParamsMap.put("vnp_Locale", "vn");
-        vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl+"?orderid="+orderId);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl + "?orderid=" + orderId);
+
+        // Định dạng thời gian với múi giờ Việt Nam
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+
+        // Lấy thời gian hiện tại theo múi giờ Việt Nam
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         String vnpCreateDate = formatter.format(calendar.getTime());
         vnpParamsMap.put("vnp_CreateDate", vnpCreateDate);
+
+        // Tính thời gian hết hạn giao dịch (15 phút sau)
         calendar.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(calendar.getTime());
         vnpParamsMap.put("vnp_ExpireDate", vnp_ExpireDate);
+
         return vnpParamsMap;
     }
+
 }
